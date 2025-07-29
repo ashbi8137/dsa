a34a3cb103996f00843e4cc47fff0eb113780d0e
Ashbin <ashbin.iitpkd@gmail.com>
2025-07-29 12:57:07 +0530

    first

---
+++ demo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter the value here: " ;
+    cin >> n;
+    cout << "The value you entered is : " << n ;
+    return 0;
+}
