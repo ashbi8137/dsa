f93a0029376dd467abaa09622a44b4f2943348aa
ashbi8137 <ashputhusseri@gmail.com>
2025-08-14 11:48:05 +0530

    commanted the changes i made

--- leetcode_day1/largest3SameDigitNumber-string.cpp
+++ leetcode_day1/largest3SameDigitNumber-string.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 string largestStringInteger(string num) {
     string ans = "";
+    //loop through the string 
+    //each i have the length of 3
+    
+    
     for (int i = 0; i <= num.size() - 3; i++) {
+        //check the 3 consecutive numbers are same
         if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+            //if yes pick the substring 
             string sub = num.substr(i, 3);
+            //return the substring is there or not
             if (sub > ans) {
                 ans = sub;
             }
@@ -15,6 +22,7 @@ string largestStringInteger(string num) {
 }
 
 int main() {
+    //give a string input
     string num;
     cin >> num;
     cout << largestStringInteger(num) << endl;
