56f73a2277e2e6beeb8c396301c58df67c881194
ashbi8137 <ashputhusseri@gmail.com>
2025-08-18 11:54:23 +0530

    dynamicinput

--- leetcode_day2/dynamicinput.cpp
+++ leetcode_day2/dynamicinput.cpp
@@ -13,3 +13,31 @@ Output:
 50 40 30 20 10
 */
 
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n; 
+    cout << "Input:" << endl ;
+    cin >> n ;
+    vector<int> v(n);
+    cout << "Enter the elements :" << endl ;
+    for( int i =0 ; i< n ;i++){
+        cin >> v[i] ;
+    }
+    for( int i = n -1 ; i>=0 ;i--){
+        cout << v[i] << " " ;
+    }
+}
+
+
+/*
+Use push_back()
+
+vector<int> v;
+for (int i = 0; i < n; i++) {
+    int num;
+    cin >> num;
+    v.push_back(num);
+}
+*/
